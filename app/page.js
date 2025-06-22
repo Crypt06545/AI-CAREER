3a182a1257b0d2a1b64b8ce93b43ac5906dfe26a
Crypt06545 <mehedihasansagor2005@gmail.com>
2025-06-22 14:16:43 +0600

    add features section

--- app/page.js
+++ app/page.js
@@ -1,11 +1,61 @@
+"use client";
 import HeroSection from "@/components/HeroSection";
+import { features } from "./data/features";
+import { Card, CardContent } from "@/components/ui/card";
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 export default function Home() {
+  const tl = gsap.timeline();
+  useGSAP(() => {
+    tl.from(".feature-section", {
+      scrollTrigger: {
+        trigger: ".feature-section",
+        markers: true,
+      },
+    });
+  }, []);
   return (
     <div>
       <div className="grid-background" />
       <main className="relative z-10">
-        <HeroSection/>
+        <HeroSection />
+
+        <section className="feature-section w-full py-12 md:py-24 lg:py-32 bg-background">
+          <div className="container mx-auto px-4 md:px-6">
+            <h2 className="text-3xl font-bold tracking-tighter text-center mb-12">
+              {/* tracking-tighter - used to word gap*/}
+              Powerful Features For Your Career Groth
+            </h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 max-w-6xl mx-auto">
+              {features.map((feature, indx) => (
+                <Card
+                  key={indx}
+                  className={
+                    "border-2 hover:border-primary cursor-pointer transition-colors duration-300"
+                  }
+                >
+                  <CardContent
+                    className={"pt-6 text-center flex flex-col items-center"}
+                  >
+                    <div className="flex flex-col items-center justify-center">
+                      <div>{feature?.icon}</div>
+                      <h3 className="text-xl font-bold mb-2">
+                        {feature?.title}
+                      </h3>
+                      <p className="text-muted-foreground">
+                        {feature?.description}
+                      </p>
+                    </div>
+                  </CardContent>
+                </Card>
+              ))}
+            </div>
+          </div>
+        </section>
       </main>
     </div>
   );
--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -1,11 +1,24 @@
+"use client";
 import Link from "next/link";
 import React from "react";
 import { Button } from "./ui/button";
 
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+
 const HeroSection = () => {
+  const tl = gsap.timeline();
+  useGSAP(() => {
+    tl.from(".hero", {
+      y: -20,
+      opacity: 0,
+      delay: 0.25,
+      duration: 0.75,
+    });
+  }, []);
   return (
-    <div className="container mx-auto pt-36 pb-10 px-4 text-center max-w-4xl">
-      <div className="space-y-6">
+    <section className="container mx-auto pt-36 min-h-screen pb-10 px-4 text-center max-w-4xl">
+      <div className="hero space-y-6">
         {/* Gradient Heading */}
         <h1 className="text-5xl md:text-6xl font-extrabold leading-tight text-white drop-shadow-lg">
           Your AI Career Coach for <br />
@@ -31,7 +44,7 @@ const HeroSection = () => {
           </Link>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
